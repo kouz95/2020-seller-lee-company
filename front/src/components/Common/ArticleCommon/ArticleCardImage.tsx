48cda581deccf554ef0b3bad5b30652fd8f2c4f2
Eun Seok <jeseok95@gmail.com>
2020-08-04 14:31:14 +0900

    refactor: 컴포넌트 분리

---
+++ front/src/components/Common/ArticleCommon/ArticleCardImage.tsx
@@ -0,0 +1,32 @@
+/**
+ * @author joseph415
+ */
+
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+import { innerContainerMargin } from "./ArticleCard";
+
+interface IArticleCardImage {
+  thumbnail: string;
+}
+
+export default function ArticleCardImage({ thumbnail }: IArticleCardImage) {
+  return (
+    <View style={styles.imageContainer}>
+      <Image style={styles.image} source={{ uri: thumbnail }} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    flex: 4.5,
+    margin: innerContainerMargin,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+    borderRadius: 15,
+  },
+});
