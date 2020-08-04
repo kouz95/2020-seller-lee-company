613bf2746b009bd7ea711a969c4589982daa6158
Eun Seok <jeseok95@gmail.com>
2020-08-04 17:38:07 +0900

    refactor: MemberResponse & ArticleResponse refactoring

--- back/src/main/java/sellerlee/back/member/application/MemberResponse.java
+++ back/src/main/java/sellerlee/back/member/application/MemberResponse.java
@@ -3,6 +3,7 @@ package sellerlee.back.member.application;
 import sellerlee.back.member.domain.Member;
 
 public class MemberResponse {
+    private Long id;
     private String avatar;
     private String nickname;
     private Double score;
@@ -10,14 +11,20 @@ public class MemberResponse {
     private MemberResponse() {
     }
 
-    private MemberResponse(String avatar, String nickname, Double score) {
+    private MemberResponse(Long id, String avatar, String nickname, Double score) {
+        this.id = id;
         this.avatar = avatar;
         this.nickname = nickname;
         this.score = score;
     }
 
     public static MemberResponse of(Member member) {
-        return new MemberResponse(member.getAvatar(), member.getNickname(), member.getScore());
+        return new MemberResponse(member.getId(), member.getAvatar(), member.getNickname(),
+                member.getScore());
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getAvatar() {
