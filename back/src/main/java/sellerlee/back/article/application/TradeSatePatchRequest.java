9359207811ca8ba88257d559a0f71198ee7c2852
Eun Seok <jeseok95@gmail.com>
2020-08-11 09:18:09 +0900

    feat: test 작성

---
+++ back/src/main/java/sellerlee/back/article/application/TradeSatePatchRequest.java
@@ -0,0 +1,22 @@
+package sellerlee.back.article.application;
+
+public class TradeSatePatchRequest {
+    private Long id;
+    private String tradeState;
+
+    public TradeSatePatchRequest() {
+    }
+
+    public TradeSatePatchRequest(Long id, String tradeState) {
+        this.id = id;
+        this.tradeState = tradeState;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTradeState() {
+        return tradeState;
+    }
+}
